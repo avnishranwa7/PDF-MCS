600907338e5c0b0b459d37785509db813f989020
Avnish Ranwa <avnish.ranwa@robomq.io>
2023-06-11 01:42:34 +0530

    PDF upload

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { firebaseConfig } from "./firebaseConfig";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDoa2alW7uOe3e8p_okxui2BBfB_hXudcM",
-  authDomain: "pdf-mcs-c169a.firebaseapp.com",
-  projectId: "pdf-mcs-c169a",
-  storageBucket: "pdf-mcs-c169a.appspot.com",
-  messagingSenderId: "935169902302",
-  appId: "1:935169902302:web:7af244e8c026194a55a44f",
-  measurementId: "G-R5LCL24XPC"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const storage = getStorage();
--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,13 +1,160 @@
 import styled from "styled-components";
+import { useState } from "react";
+import { storage } from "../firebase";
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+
+import uploadIcon from "../assets/uploadIcon.svg";
+import fileIcon from "../assets/fileIcon.svg";
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
 
 export const UploadPage = () =>{
+    const [submitButtonClicked, setSubmitButtonClicked] = useState(false);
+    const [pdfUrl, setPdfUrl] = useState("");
+    const [progresspercent, setProgresspercent] = useState(0);
+    const [file, setFile] = useState(null);
+
+    console.log(file);
+
+    const handleFile = (e) =>{
+        e.preventDefault();
+        if(e.target.files[0]){
+            setFile(e.target.files[0])
+        }
+    }
+
+    const handleSubmit = (e) =>{
+        if(file){
+            setSubmitButtonClicked(true);
+            const storageRef = ref(storage, `files/${file.name}`);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+            uploadTask.on("state_changed", (snapshot)=>{
+                const progress = Math.round(snapshot.bytesTransferred / snapshot.totalBytes)*100;
+                setProgresspercent(progress);
+            }, (error)=>{
+                console.log(error.code);
+            }, ()=>{
+                setSubmitButtonClicked(false);
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>{
+                    setPdfUrl(downloadURL);
+                    setProgresspercent("");
+                    setFile(null);
+                });
+            });
+        }
+    }
+
     return(
         <UploadDiv>
-            Upload page
+            <h1>Upload PDF</h1>
+            {!file &&
+                <Form>
+                    <label for="file-input">
+                        <IconButton>
+                            <img src={uploadIcon} />
+                        </IconButton>
+                    </label>
+                    <input id="file-input" type="file" accept="application/pdf" onChange={handleFile} />
+                </Form>
+            }
+            {file &&
+            <Form>
+                {submitButtonClicked && progresspercent!==100 &&
+                    <Box sx={{ width: '100%' }}>
+                        <LinearProgress />
+                    </Box>
+                }
+                <IconButton>
+                    <img src={fileIcon} />
+                </IconButton>
+                {file.name}
+                <button class="btn btn-outline-success" onClick={handleSubmit}>Upload file</button>
+            </Form>
+        }
+
         </UploadDiv>
     )
 }
 
-const UploadDiv = styled.div`
-    padding-top: 56px;
+const IconButton = styled.div`
+    margin-top: 7rem;
+    cursor: pointer;
+    margin-bottom: 0.5rem;
 `
+
+const Form = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+
+    button{
+        margin-top: 0.5rem;
+    }
+
+    input{
+        display: none;
+    }
+`
+
+const UploadDiv = styled.div`
+    padding-top: 7rem;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+`
+
+
+
+
+// import { useState } from "react";
+// import { storage } from '../firebase';
+// import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+
+// export function UploadPage() {
+//   const [imgUrl, setImgUrl] = useState(null);
+//   const [progresspercent, setProgresspercent] = useState(0);
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault()
+//     const file = e.target[0]?.files[0]
+//     if (!file) return;
+//     const storageRef = ref(storage, `files/${file.name}`);
+//     const uploadTask = uploadBytesResumable(storageRef, file);
+
+//     uploadTask.on("state_changed",
+//       (snapshot) => {
+//         const progress =
+//           Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+//         setProgresspercent(progress);
+//       },
+//       (error) => {
+//         alert(error);
+//       },
+//       () => {
+//         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+//           setImgUrl(downloadURL)
+//         });
+//       }
+//     );
+//   }
+
+//   return (
+//     <div style={{paddingTop: "100px"}}>
+//       <form onSubmit={handleSubmit} className='form'>
+//         <input type='file' />
+//         <button type='submit'>Upload</button>
+//       </form>
+//       {
+//         !imgUrl &&
+//         <div className='outerbar'>
+//           <div className='innerbar' style={{ width: `${progresspercent}%` }}>{progresspercent}%</div>
+//         </div>
+//       }
+//       {
+//         imgUrl &&
+//         <img src={imgUrl} alt='uploaded file' height={200} />
+//       }
+//     </div>
+//   );
+// }
